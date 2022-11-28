c9d1b2707c8a0e9ce7ea104fba5d7c30f1f97066
younes achachi <younes.achachi1@gmail.com>
2022-11-28 17:24:03 +0100

    cheange Titre

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
 	return (
 		<ApolloProvider client={client} class="flex">
 			<div id="main" className="flex w-[100%] space-x-40 mx-28 pt-[10%] ">
-				<p class=" mt-9 text-3xl  text-amber-800   italic"> GraphQl Books Rendering: </p>
+				<p class=" mt-9 text-3xl  text-amber-800   italic"> GraphQl Books Rendering with netlifly: </p>
 				<div class="block">
 					<BookList class="flex mt-2 border-solide" />
 				</div>
