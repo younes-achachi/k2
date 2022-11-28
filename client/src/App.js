91247c57da0e1c88f1412dacd4ebacb41734fcdd
younes achachi <younes.achachi1@gmail.com>
2022-11-28 16:43:11 +0100

    fupload to git

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ const client = new ApolloClient({
 	uri: 'http://localhost:4000/graphql',
 	cache: new InMemoryCache()
 });
+console.log('v');
 function App() {
 	return (
 		<ApolloProvider client={client} class="flex">
